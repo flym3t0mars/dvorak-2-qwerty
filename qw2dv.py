3b235eae3f2f379f4492aaecd0d95be4bbaaf0f2
Carlos Gudino <cgudino95@me.com>
2022-02-19 09:01:51 -0800

    keyboard loop

--- qw2dv.py
+++ qw2dv.py
@@ -1,5 +1,7 @@
 #! /bin/python
 import sys
+from os import system as s
+from time import sleep as zz
 
 qwerty = ['q','w','e','r','t','y','u','i','o','p','[',']','\\',
 	   'a','s','d','f','g','h','j','k','l',';',"'",
@@ -12,7 +14,13 @@ dvorak = ["'",',','.','p','y','f','g','c','r','l','/','=','\\',
 		'\n'
 	 ]
 
+# I want to simulate the press with these ascii character
+# also redraw the keyboard with the key pressed
+# but refresh the page without losing the message typed
+# real time key press
 
+
+pressed = '■'
 
 def oskb():
 	with open('tt.md','r')as r:
@@ -22,8 +30,28 @@ def oskb():
 				pass
 			else:
 				print(lines,end='')
+		
 
-		print(keyboard)
+		# print(keyboard)
+
+def pressed(msg='null'):
+	while True:
+		oskb()
+		zz(0.01)
+		clear()
+	
+
+
+def clear():
+	try:
+		if sys.platform != 'linux':
+			s('cls')
+		else:
+			s('clear')
+	except Exception as e:
+		print(e)
+	else:
+		pass
 
 
 def file(b='null'):
@@ -69,6 +97,7 @@ def opt():
 
 def main():
 #	oskb()
+	pressed()
 	ask=input('do you want to use a file (y/n): ')
 	if ask == 'y':
 		file(sys.argv[1])
